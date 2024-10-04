3feabbc8ec5e63dde04ad5d923b858949463a88a
jiyoon607 <6judy611@gmail.com>
2024-10-04 23:10:33 +0900

    ✨ Feat: 일반JWT 로그인 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
-from django.contrib import admin
 from django.urls import path, include
 from .views import *
 
 app_name = 'accounts'
 
 urlpatterns = [
+    #일반 로그인 / 회원가입
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    
     path('allauth/', include('allauth.urls')),
 
     # 카카오 로그인
--- linenow/settings.py
+++ linenow/settings.py
@@ -79,6 +79,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ),
     'EXCEPTION_HANDLER': 'utils.exceptions.custom_exception_handler',
 }
