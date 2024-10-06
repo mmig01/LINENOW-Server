c2a91a4592b6e56d2e1d36ae9a9656f29efd1268
JongbeomLee623 <jambon23@dgu.ac.kr>
2024-10-06 23:35:53 +0900

    🐛 Fix: urls.py trailing slash 수정 및 accounts url 변경

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from .views import *
+from dj_rest_auth.views import LoginView, LogoutView
 
 app_name = 'accounts'
 
 urlpatterns = [
     # 일반 로그인 / 회원가입
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),    
-    path('allauth/', include('allauth.urls')),
+    path('accounts/login', LoginView.as_view(), name='login'),
+    path('accounts/logout', LogoutView.as_view(), name='logout'),
+    path('accounts/registration', include('dj_rest_auth.registration.urls')),
+    # path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    # path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),    
+    # path('allauth/', include('allauth.urls')),
 
     # 회원 탈퇴
-    path('accounts/withdraw/', WithdrawUserView.as_view(), name='withdraw'),
+    path('accounts/withdraw', WithdrawUserView.as_view(), name='withdraw'),
 ]
