efe2e03b718525f2eaa53290fa779052dfafa882
jiyoon607 <lakrlakr@dgu.ac.kr>
2025-04-01 01:05:57 +0900

    ✨ Feat: booth모델 수정 및 부스 개수api

--- booth/models.py
+++ booth/models.py
@@ -2,56 +2,52 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 def image_upload_path(instance, filename):
-    event_id = instance.booth.event.id
-    booth_id = instance.booth.id
-    return f'event_{event_id}/booth_{booth_id}/{filename}'
+    booth_id = instance.booth.booth_id
+    return f'booth_{booth_id}/{filename}'
 
-class Event(models.Model):
+class Festival(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, verbose_name="행사명")
+    fastival_name = models.CharField(max_length=100, verbose_name="행사명")
     
     def __str__(self):
-        return self.name
+        return self.fastival_name
     
 class Booth(models.Model):
-    OPERATED_STATUS = [
-        ('not_started', 'Not Started'), # DB에 저장되는 값, 사용자에게 보여지는 값 순
-        ('operating', 'Operating'),
-        ('finished', 'Finished'),
-        ('paused', 'Paused')
+    STATUS_CHOICES = [
+        ('not_started', 'not_started'), # DB에 저장되는 값, 사용자에게 보여지는 값 순
+        ('operating', 'operating'),
+        ('finished', 'finished'),
+        ('paused', 'paused')
     ]
 
-    id = models.AutoField(primary_key=True)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="booths") 
-    name = models.CharField(max_length=100, verbose_name="부스명")
-    description = models.TextField(verbose_name="부스 설명")
-    caution = models.TextField(verbose_name="부스 유의사항")
-    location = models.CharField(max_length=255, verbose_name="부스 위치")
-    is_operated = models.CharField(max_length=100, choices=OPERATED_STATUS, verbose_name="운영 여부")
-    open_time = models.DateTimeField(verbose_name="시작 시간")
-    close_time = models.DateTimeField(verbose_name="마감 시간")
-
-    def save(self, *args, **kwargs):
-        if self.is_operated == 'operating' and self.open_time is None:
-            self.open_time = timezone.now()
-        elif self.is_operated == 'finished' and self.close_time is None:
-            self.close_time = timezone.now()
-        super().save(*args, **kwargs)
+    booth_id = models.AutoField(primary_key=True)
+    booth_name = models.CharField(max_length=100, verbose_name="부스명")
+    booth_description = models.TextField(verbose_name="부스 설명")
+    booth_location = models.CharField(max_length=255, verbose_name="부스 위치")
+    booth_start_time = models.DateTimeField(verbose_name="운영 시작 시간")
+    booth_end_time = models.DateTimeField(verbose_name="운영 마감 시간")
+    booth_notice = models.TextField(verbose_name="부스 공지")
+    booth_latitude = models.CharField(max_length=255, verbose_name="부스 위치 위도")
+    booth_longitude = models.CharField(max_length=255, verbose_name="부스 위치 경도")
+    operating_status = models.CharField(max_length=100, choices=STATUS_CHOICES, verbose_name="부스 운영 상태")
+    current_watiting_num = models.IntegerField(verbose_name="최신 대기 번호")
 
     def __str__(self):
-        return self.name
+        return self.booth_name
     
-class BoothMenu(models.Model):
+class Booth_Menu(models.Model):
+    menu_id = models.AutoField(primary_key=True)
     booth = models.ForeignKey(Booth, on_delete=models.CASCADE, related_name="menus")
-    name = models.CharField(max_length=100, verbose_name="메뉴 이름")
-    price = models.IntegerField(verbose_name="가격")
+    menu_name = models.CharField(max_length=100, verbose_name="메뉴 이름")
+    menu_price = models.IntegerField(verbose_name="메뉴 가격")
 
     def __str__(self):
-        return f'{self.name} - {int(self.price):,}원'
+        return f'{self.menu_name} - {int(self.menu_price):,}원'
     
-class BoothImage(models.Model):
+class Booth_Image(models.Model):
+    booth_image_id = models.AutoField(primary_key=True)
     booth = models.ForeignKey(Booth, on_delete=models.CASCADE, related_name="boothimages")
-    image = models.ImageField(upload_to=image_upload_path, blank=True, null=True)
+    booth_image = models.ImageField(upload_to=image_upload_path, blank=True, null=True, verbose_name="사진")
     
     def __str__(self):
-        return f'{self.booth.name} - {self.id}'
+        return f'{self.booth.booth_name} - {self.booth_image_id}'
