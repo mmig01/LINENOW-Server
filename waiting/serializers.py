67c2442db9898d9542b05c549e9e24b5f70e8fc7
che-hyeon <ghkdcogus03@naver.com>
2025-04-13 01:02:07 +0900

    Feat: 대기 booth 정보 추가 & migrate 수정

--- waiting/serializers.py
+++ waiting/serializers.py
@@ -6,22 +6,63 @@ from django.utils import timezone
 from datetime import timedelta
 
 # 대기에 사용될 부스 serializer - 부스 모델 수정 후 작성 예정
+class WaitingBoothListSerializer(serializers.ModelSerializer):
+    booth_thumbnail = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Booth
+        fields = [
+            'booth_id',
+            'booth_name',
+            'booth_location',
+            'booth_thumbnail'
+        ]
+    
+    def get_booth_thumbnail(self, obj):
+        request = self.context.get('request')
+        thumbnail = obj.booth_images.first()
+        if thumbnail and request:
+            return request.build_absolute_uri(thumbnail.booth_image.url)
+        return ''
+    
+class WaitingBoothDetailSerializer(serializers.ModelSerializer):
+    booth_thumbnail = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Booth
+        fields = [
+            'booth_id',
+            'booth_name',
+            'booth_location',
+            'booth_latitude',
+            'booth_longitude',
+            'booth_thumbnail'
+        ]
+    
+    def get_booth_thumbnail(self, obj):
+        request = self.context.get('request')
+        thumbnail = obj.booth_images.first()
+        if thumbnail and request:
+            return request.build_absolute_uri(thumbnail.booth_image.url)
+        return ''
 
 # 대기 리스트 serializer
 class WaitingListSerializer(serializers.ModelSerializer):
     waiting_team_ahead = serializers.SerializerMethodField()
+    booth_info = serializers.SerializerMethodField()
 
     class Meta:
         model = Waiting
         fields = [
             'waiting_id',
-            # 'booth_info',
+            'booth_info',
             'waiting_team_ahead',
             'waiting_status',
             'created_at',
             'confirmed_at',
             'canceled_at',
         ]
+    
 
     def get_waiting_team_ahead(self, obj):
         return Waiting.objects.filter(
@@ -29,6 +70,10 @@ class WaitingListSerializer(serializers.ModelSerializer):
             created_at__lt=obj.created_at,
             waiting_status='waiting'
         ).count()
+    
+    def get_booth_info(self, obj):
+        request = self.context.get('request')
+        return WaitingBoothListSerializer(obj.booth, context={'request': request}).data
 
 
 class WaitingDetailSerializer(serializers.ModelSerializer):
@@ -42,7 +87,7 @@ class WaitingDetailSerializer(serializers.ModelSerializer):
             'person_num',
             'waiting_team_ahead',
             'waiting_status',
-            # 'booth_info',
+            'booth_info',
             'created_at',
             'confirmed_at',
             'canceled_at'
@@ -54,3 +99,7 @@ class WaitingDetailSerializer(serializers.ModelSerializer):
             created_at__lt=obj.created_at,
             waiting_status='waiting'
         ).count()
+    
+    def get_booth_info(self, obj):
+        request = self.context.get('request')
+        return WaitingBoothDetailSerializer(obj.booth, context={'request': request}).data
