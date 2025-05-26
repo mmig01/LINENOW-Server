205718467107d37a26c2e2d942d4b39b9279167c
jiyoon607 <lakrlakr@dgu.ac.kr>
2025-05-27 00:30:44 +0900

    Feat: 어드민용 부스 조회 api추가

--- booth/urls.py
+++ booth/urls.py
@@ -12,11 +12,15 @@ booth_waiting_router = routers.SimpleRouter(trailing_slash=False)
 booth_waiting_router.register(r'booths-waiting', BoothWaitingStatusViewSet, basename='booth-waiting')
 
 GDGbooth_router = routers.SimpleRouter(trailing_slash=False)
-GDGbooth_router.register(r'gdg-booths', GDGBoothViewSet, basename='booth')
+GDGbooth_router.register(r'gdg-booths', GDGBoothViewSet, basename='gdg-booth')
+
+booth_for_admin_router = routers.SimpleRouter(trailing_slash=False)
+booth_for_admin_router.register(r'booths-for-admin', BoothForAdminViewSet, basename='booth-for-admin')
 
 urlpatterns = [
     path('', include(booth_router.urls)),
     path('', include(GDGbooth_router.urls)),
     path('', include(booth_waiting_router.urls)),
+    path('', include(booth_for_admin_router.urls)),
     path('upload-booth-data', BoothDataView.as_view(), name="upload_booth_data")
 ]
